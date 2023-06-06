6aa4b6a5fb30e284fb1e1fa0fa17bb1b0818d467
Dhitaaprilia <112038650+Dhitaaprilia@users.noreply.github.com>
2023-06-07 00:15:19 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -228,8 +228,6 @@ with implementation:
         Smoke = st.number_input('Smoking')
         Alco = st.number_input('Alcohol')
         Active = st.number_input('Physical Activity')
-        model = st.selectbox('Pilihlah model yang akan anda gunakan untuk melakukan prediksi?',
-                ('K-NN'))
 
         prediksi = st.form_submit_button("Submit")
         if prediksi:
